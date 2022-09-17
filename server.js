6e5244d38d7519df0652f9b775050805d7f5de73
jiahaopan-Deakin <90379329+jiahaopan-Deakin@users.noreply.github.com>
2022-09-17 16:35:13 +1000

    Create mongoose connection and create Doctor collection

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
-
+const mongoose = require("mongoose");
 const app = express();
-
 let database;
-
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-
 // Add database connection
 const uri =
-  'mongodb+srv://getdocteam:sit725@mycluster.uawhx6v.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri, { useNewURLParser: true });
+  'mongodb+srv://getdocteam:sit725@mycluster.uawhx6v.mongodb.net/getdoc?retryWrites=true&w=majority';
+// connect mongoose to call the schema function
+mongoose.connect(uri, {useNewUrlParser:true}, (err) =>{
+  if (err){
+    throw err;
+  }
+  else{
+    console.log("Mongoose Connect successfully");
+  }
+})
+  // Connect to MongoDB
+  MongoClient.connect(uri, { useNewURLParser: true }, (err, result) => {
+    if (err) throw err;
+    database = result.db('getdoc');
+    console.log('MongoDB connection successful');
+  });
 
 const getCollection = (collection, res) => {
   database
@@ -51,6 +62,44 @@ app.get('/api/patientSched', (req, res) => {
   getCollection('patientSched', res);
 });
 
+//Doctor collection created
+const DoctorClassSchema = new mongoose.Schema({
+  _email: {type: String},
+  _name: {type: String},
+  _password: {type: String},
+
+  _comments: {type: Array},
+  _experience: {type: Array, 
+      _HospitalName: {type: String},
+      _Duration: {type: String}
+  },
+  _education: {type: Array,
+      _UniName: {type: String},
+      _Degree: {type: String}
+    },
+  _AvailableTime: {type: Array,
+      _date: {type: Date},
+      _from: {type: String},
+      _to: {type: String},
+      _Status: {type: Boolean}}
+
+},{collection:'DoctorClass'})
+
+const Doctor = mongoose.model('Doctor', DoctorClassSchema)
+
+const doctor = new Doctor({
+  _email: "123@gmail.com",
+  _name: "Li Lei",
+  _password: "123456",
+  _comments: ["Nice doctor", "Pretty nice experience"],
+  _experience: [{_HospitalName: "RenMin", _Duration: "5 years"},
+               {_HospitalName: "No2", _Duration: "7 years"}],
+  _education: [{_UniName: "Deakin", _Degree: "PHD"},
+               {_UniName: "Monash", _Degree: "PHD"}],
+  _AvailableTime: [{_date: "15/09/2022", _from:"9am", _to:"9.30am", _Status: false}]
+})
+doctor.save();
+
 // Login
 app.post('/login', (req, res) => {
   let email = req.body.email;
@@ -75,11 +124,4 @@ const port = process.env.port || 3000;
 
 app.listen(port, () => {
   console.log('App running at http://localhost:' + port);
-
-  // Connect to MongoDB
-  MongoClient.connect(uri, { useNewURLParser: true }, (err, result) => {
-    if (err) throw err;
-    database = result.db('getdoc');
-    console.log('MongoDB connection successful');
-  });
 });
