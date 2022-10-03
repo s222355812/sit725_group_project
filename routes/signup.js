1d155efd26e82f0be4f20229a260c220fccc4fe3
s222355812 <109494663+s222355812@users.noreply.github.com>
2022-10-03 12:01:55 +1100

    sign up form updated in routes

--- routes/signup.js
+++ routes/signup.js
@@ -1,76 +1,79 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const mongoose = require('mongoose');
-
-let { getDB } = require('../dbConnect');
+var mongoose = require('mongoose');
+let {
+  getDB
+} = require('../dbConnect');
 let database;
 getDB.then((result) => {
   database = result;
 });
 
+//Patients schema
+const PatientClassSchema = new mongoose.Schema(
+  {
+    _email: { type: String }, 
+    _name: { type: String },
+    _password: { type: String },
+    _sex: { type: String },
+    _dob: { type: String },
+    _age: { type: String },
+    _phone: { type: String },
+    _medicalHistory: {
+      type: Array,
+      _condition: { type: String },
+      _year: { type: String },
+    },
+    _ratings: {
+      type: Array,
+      _starCount: { type: String },
+      _comment: { type: String },
+    },
+    _schedule: {
+      type: Array,
+      _date: { type: Date },
+      _from: { type: String },
+      _to: { type: String },
+      _status: { type: String },
+      _doctorName: { type: String },
+    },
+  },
+  { collection: 'Patient' }
+);
+
 router.post('/signup', (req, res) => {
-  let email = req.body.email;
   let fname = req.body.fname;
-  let lname = req.body.fname;
-  let password = req.body.password;
+  let lname = req.body.lname;
+  let email = req.body.email;
   let sex = req.body.sex;
   let dob = req.body.dob;
+  let password = req.body.password;
   let phone = req.body.phoneCode + req.body.phone;
-  let schedule = {};
   const birthYear = new Date(dob).getFullYear();
   const currentYear = new Date().getFullYear();
   const age = currentYear - birthYear;
   const Patient = mongoose.model('Patient', PatientClassSchema);
 
   const patientData = new Patient({
-    _user: 'patient',
-    _email: `${email}`,
-    _fname: `${fname}`,
-    _lname: `${lname}`,
-    _password: `${password}`,
-    _picture: 'images/profile.jpg',
-    _sex: `${sex}`,
-    _dob: `${dob}`,
     _age: `${age}`,
+    _dob: `${dob}`,
+    _email: `${email}`,
+    _name: `${fname}` + " " + `${lname}`,
     _phone: `${phone}`,
-    _medicalHistory: [{}],
-    _schedule: {},
-    _patientRatings: {
-      'Doctor Name 1': [
-        {
-          commentDate: 'DD/MM/YYYY',
-          docPicture: 'images/sample-picture-doctor.webp',
-          docProfileLink: '!#',
-          rating: '3.5',
-          patientComment:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sagittis scelerisque tincidunt. Praesent blandit tempor vestibulum.',
-        },
-      ],
-    },
+    _password: `${password}`,
+    _sex: `${sex}`,
+    _schedule: [{
+      _date: '01/01/2001',
+      _from: '10 AM',
+      _to: '11 AM',
+      _status: 'BooKed',
+      _doctorName: 'ABC XYZ',
+    }]
   });
 
   database.collection('Patient').insertOne(patientData);
-  res.redirect('/login.html');
+  res.sendFile(path.resolve('public/login.html'));
 });
-
-const PatientClassSchema = new mongoose.Schema(
-  {
-    _user: { type: String },
-    _email: { type: String },
-    _fname: { type: String },
-    _lname: { type: String },
-    _password: { type: String },
-    _picture: { type: String },
-    _sex: { type: String },
-    _dob: { type: String },
-    _age: { type: String },
-    _phone: { type: String },
-    _medicalHistory: { type: Array },
-    _schedule: { type: Object },
-    _patientRatings: { type: Object },
-  },
-  { collection: 'Patient', minimize: false }
-);
 
 module.exports = router;
