2f1d2163d2bda8d7eaf656afe74bfda30eb544cf
s222355812 <109494663+s222355812@users.noreply.github.com>
2022-10-04 21:41:13 +1100

    model updated

---
+++ Model/PatientModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+//Patients schema
+const PatientClassSchema = new mongoose.Schema(
+    {
+        _user: { type: String },
+        _email: { type: String },
+        _fname: { type: String },
+        _lname: { type: String },
+        _password: { type: String },
+        _picture: { type: String },
+        _sex: { type: String },
+        _dob: { type: String },
+        _age: { type: String },
+        _phone: { type: String },
+        _medicalHistory: { type: Array },
+        _schedule: { type: Object },
+        _patientRatings: { type: Object },
+      },
+      { collection: 'Patient', minimize: false }
+    );
+    module.exports = {
+        DoctorClassSchema
+    }
